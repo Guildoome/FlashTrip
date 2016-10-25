d0cf5ac4595fb5957e59f5d5f0cffe6db19a5eb0
Guildoome <delbar.guillaume@gmail.com>
2016-10-25 16:39:10 +0200

    qr non fonctionnel

---
+++ app/src/main/java/com/example/a34011_73_07/flashtrip/Description.java
@@ -0,0 +1,13 @@
+package com.example.a34011_73_07.flashtrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Description extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description);
+    }
+}
---
+++ app/src/main/java/com/example/a34011_73_07/flashtrip/PointsOverlayView.java
@@ -0,0 +1,8 @@
+package com.example.a34011_73_07.flashtrip;
+
+/**
+ * Created by 34011-73-07 on 25/10/2016.
+ */
+
+public class PointsOverlayView {
+}
